4789264e1ce6b82c127b19fa1b2e2f67874b0145
Saurav Das <sauravd69@gmail.com>
2022-05-25 22:56:26 -0500

    extended SpringBootServletInitializer to deploy war in tomcat

--- src/main/java/com/sd/netflix/NetflixApplication.java
+++ src/main/java/com/sd/netflix/NetflixApplication.java
@@ -7,29 +7,36 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
 
 @PropertySource("file:C:\\\\Code\\\\netflix-${env}.properties")
 @SpringBootApplication
 @Slf4j
-public class NetflixApplication {
+public class NetflixApplication extends SpringBootServletInitializer {
 
-	@Value("${env}")
-	private String env;
+    @Value("${env}")
+    private String env;
 
-	public static void main(String[] args) {
-		SpringApplication.run(NetflixApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(NetflixApplication.class, args);
+    }
 
-	@Bean
-	CommandLineRunner run (MovieRepository movieRepository) {
-		return  args -> {
-			log.info("env is: {}", env);
-			if (env.equals("dev")) {
-				log.info("inserting data into dev DB");
-				movieRepository.save(new MovieEntity(null, "Dont look up", "Movie", "Sci-fi", 2021, 4.50));
-			}
-		};
-	}
+    @Bean
+    CommandLineRunner run(MovieRepository movieRepository) {
+        return args -> {
+            log.info("env is: {}", env);
+            if (env.equals("dev")) {
+                log.info("inserting data into dev DB");
+                movieRepository.save(new MovieEntity(null, "Dont look up", "Movie", "Sci-fi", 2021, 4.50));
+            }
+        };
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(NetflixApplication.class);
+    }
 }
