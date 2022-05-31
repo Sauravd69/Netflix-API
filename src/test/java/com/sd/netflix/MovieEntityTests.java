e30344b75b75a7b7ca1b285786fbec04a579d415
Saurav Das <sauravd69@gmail.com>
2022-05-31 16:38:48 -0500

    added jacoco-4

--- src/test/java/com/sd/netflix/MovieEntityTests.java
+++ src/test/java/com/sd/netflix/MovieEntityTests.java
@@ -2,9 +2,11 @@ package com.sd.netflix;
 
 import com.sd.netflix.entity.MovieEntity;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@SpringBootTest
 public class MovieEntityTests {
 
     MovieEntity record1 = new MovieEntity(null, "Dont look up", "Movie", "Sci-fi", 2021, 4.50);
